3cb26a35b5fa9df147ec6825f32f5297ee791a50
saneenKP <saneenkp2000@gmail.com>
2021-11-24 01:47:40 +0530

    UI almost completed

--- app.js
+++ app.js
@@ -1,62 +1,72 @@
-const express = require('express')
-const http = require('http')
-//const { callType } = require('./public/js/constants.js')
+const express = require("express");
+const http = require("http");
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-const app = express()
-const server = http.createServer(app)
-const io = require('socket.io')(server)
+const app = express();
+const server = http.createServer(app);
+const io = require("socket.io")(server);
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
-app.get('/',(req , res)=>{
-    res.sendFile(__dirname + '/public/index.html')
-})
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
-let connectedPeers = []
+let connectedPeers = [];
 
-io.on('connection', (socket) => {
-    connectedPeers.push(socket.id)
+io.on("connection", (socket) => {
+  connectedPeers.push(socket.id);
+
+  socket.on("pre-offer", (data) => {
+    console.log("pre-offer-came");
+    const { calleePersonalCode, callType } = data;
+    console.log(calleePersonalCode);
     console.log(connectedPeers);
-    
-    // socket.on('pre-offer', (data)=>{
+    const connectedPeer = connectedPeers.find(
+      (peerSocketId) => peerSocketId === calleePersonalCode
+    );
 
-    //     const {calleePersonalCode ,callType } = data
-    //     const connectedPeer = connectedPeers.find((socketId)=>{
-        
-    //         peerSocketId == calleePersonalCode
-    //     })
-    //     if(connectedPeer) {
-    //         const data = {
-    //             callerSocketId: socket.id,
-    //             callType,
-    //         }
-    //         io.to(calleePersonalCode).emit('pre-offer',data)
-    //     }
+    console.log(connectedPeer);
 
-    // })
+    if (connectedPeer) {
+      const data = {
+        callerSocketId: socket.id,
+        callType,
+      };
+      io.to(calleePersonalCode).emit("pre-offer", data);
+    } else {
+      const data = {
+        preOfferAnswer: "CALLEE_NOT_FOUND",
+      };
+      io.to(socket.id).emit("pre-offer-answer", data);
+    }
+  });
 
-    socket.on('disconnect' , ()=>{
-       console.log("User Disconnected"); 
+  socket.on("pre-offer-answer", (data) => {
+    const { callerSocketId } = data;
 
-       const newConnectedPeers = connectedPeers.filter((peerSocketId)=>{
+    const connectedPeer = connectedPeers.find(
+      (peerSocketId) => peerSocketId === callerSocketId
+    );
 
-            peerSocketId !== socket.io
-       })
-       connectedPeers = newConnectedPeers;
-       console.log(connectedPeers);
-    })
-})
+    if (connectedPeer) {
+      io.to(data.callerSocketId).emit("pre-offer-answer", data);
+    }
+  });
 
-app.get('/hello',(req , res)=>{
-    res.send("this is hello section")
-})
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
 
-app.get('/helloWorld',(req , res)=>{
-    res.send("this is helloWorld section")
-})
+    const newConnectedPeers = connectedPeers.filter(
+      (peerSocketId) => peerSocketId !== socket.id
+    );
 
-server.listen(PORT , () =>{
-    console.log("listening or port :"+PORT);
-})
+    connectedPeers = newConnectedPeers;
+    console.log(connectedPeers);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
--- public/js/main.js
+++ public/js/main.js
@@ -2,13 +2,14 @@ import * as store from "./store.js"
 import * as wss from './wss.js'
 import * as webRTCHandler from './webRTCHandler.js'
 import * as constants from './constants.js'
+import * as elements from './elements.js'
 
 //Initialization of Socket io
 const socket  = io('/')
 wss.registerSocketEvents(socket)
 
 //copy link to clipboard functionality
-const personalCodeCopyButton = document.getElementById('personalCodeCopyButton')
+const personalCodeCopyButton = document.getElementById('personal_code_copy_button')
 personalCodeCopyButton.addEventListener('click' , () => {
 
     const personalCode = store.getState().socketId
@@ -16,10 +17,20 @@ personalCodeCopyButton.addEventListener('click' , () => {
 })
 
 //getting personal code and sending it as offer to chat
-const personalCodeChatButton = document.getElementById('personalCodeChatButton')
+const personalCodeChatButton = document.getElementById('personal_code_chat_button')
 personalCodeChatButton.addEventListener('click',() => {
 
     const calleePersonalCode = document.getElementById('personal_code_input').value
     const callType = constants.callType.CHAT_PERSONAL_CODE
     webRTCHandler.sendPreOffer(callType,calleePersonalCode)
+})
+
+const personalCodeVideoButton = document.getElementById('personal_code_video_button')
+personalCodeVideoButton.addEventListener('click', () => {
+
+    const calleePersonalCode = document.getElementById('personal_code_input').value
+    const callType = constants.callType.VIDEO_PERSONAL_CODE
+    webRTCHandler.sendPreOffer(callType , calleePersonalCode)
+
+
 })
